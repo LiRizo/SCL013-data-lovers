19579f8a2d7c2cd7e596bb69403a019b25e6fd40
ConnieBrito <constanza.brito.28@gmail.com>
2020-05-03 22:18:56 -0400

    cambios html, js y css

--- src/data.js
+++ src/data.js
@@ -1,9 +1,63 @@
-// estas funciones son de ejemplo
+// Creamos un nuevo array para traer los registros de la data pokémon
 
-export const example = () => {
-  return 'example';
+export const allPokeData = (dataInfo) => {
+    const newPokeArray = [];
+    for (let i = 0; i < dataInfo.length; i += 1) {
+        newPokeArray.push({
+            id: dataInfo[i].id,
+            num: dataInfo[i].num,
+            name: dataInfo[i].name,
+            img: dataInfo[i].img,
+            type: dataInfo[i].type,
+            height: dataInfo[i].height,
+            weight: dataInfo[i].weight,
+            candy: dataInfo[i].candy,
+            candy_count: dataInfo[i].candy_count,
+            egg: dataInfo[i].egg,
+            spawn_chance: dataInfo[i].spawn_chance,
+            avg_spawns: dataInfo[i].avg_spawns,
+            spawn_time: dataInfo[i].spawn_time,
+            multipliers: dataInfo[i].multipliers,
+            weaknesses: dataInfo[i].weaknesses,
+            prev_evolution: dataInfo[i].prev_evolution,
+            next_evolution: dataInfo[i].next_evolution,
+        });
+    }
+    return newPokeArray;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+//Ordena pokemon por alfabeto A-Z
+export const orderDataAz = (dataInfo) => {
+    const newPokeArray = [];
+    for (let i = 0; i < dataInfo.length; i++) {
+        newPokeArray.push(dataInfo[i]);
+    }
+    newPokeArray.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        }
+        if (a.name < b.name) {
+            return -1;
+        }
+        return 0;
+    });
+    return newPokeArray;
+};
+
+//Ordenar alfabéticamente desde Z-A
+export const orderDataZa = (dataInfo) => {
+    const newPokeArray = [];
+    for (let i = 0; i < dataInfo.length; i += 1) {
+        newPokeArray.push(dataInfo[i]);
+    }
+    newPokeArray.sort((a, b) => {
+        if (a.name > b.name) {
+            return -1;
+        }
+        if (a.name < b.name) {
+            return 1;
+        }
+        return 0;
+    });
+    return newPokeArray;
 };
--- src/main.js
+++ src/main.js
@@ -1,16 +1,61 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import pokeData from './data/pokemon/pokemon.js';
+import { allPokeData, orderDataAz, orderDataZa } from './data.js';
 
-console.log(example, data);
+const card = document.getElementById("viewCardCreate");
 
-const formulario = document.querySelector('#formulario');
-const lupa = document.querySelector('#lupa');
+//Búsqueda interna (header) 
+//const formulario = document.querySelector('#formulario');
+//const lupa = document.querySelector('#lupa');
 
-const buscar = () => {
-    console.log(formulario.value);
-    const texto = formulario.value
+//const buscar = () => {
+//console.log(formulario.value);
+//const texto = formulario.value
+//}
+
+//lupa.addEventListener('click', buscar)
+
+//Imprimir imagen gif inicial
+
+//document.write('<img src=http://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_czZXhW51UFwTg6ZOk0xclfMXKFpYNtV-J-LEPOe5V-RXrQky&usqp=CAU/>')
+
+//Arreglo para llamar a la data de pokémon
+const allPokeInfo = allPokeData(pokeData.pokemon);
+const orderPokeAz = orderDataAz(allPokeData);
+const orderPokeZa = orderDataZa(allPokeData);
+
+//Muestra tarjetas de pokémon en pantalla
+function viewAllData(dataInfo) {
+    let cardDesign = "";
+    for (let i = 0; i < dataInfo.length; i++) {
+        cardDesign += `
+        <div class="elemCard">
+        <div id='pokemon${dataInfo[i].id}' class="card${dataInfo[i].type[0]}">
+           <div class='PokeItems'> 
+               <h2>${dataInfo[i].name}</h2>
+               <h3>${dataInfo[i].num}</h3>
+               <img class="imgPokemon" src="${dataInfo[i].img}"/>
+               <h3>${dataInfo[i].type[0]}</h3>
+           </div>
+       </div>
+       </div>`;
+    }
+    card.innerHTML = cardDesign;
 }
 
-lupa.addEventListener('click', buscar)
+viewAllData(allPokeInfo);
+
+//Para ordenar alfabéticamente (a-z / z-a)
+//const menuPokedex = document.querySelector(".ordenarPokemon");
+//selectElement.addEventListener("change", (e) => {
+const resultado = '$ { e.target.value }';
+//console.log(resultado);
+if (resultado === "A-Z") {
+    viewAllData(orderPokeAz);
+}
+if (resultado === "Z-A") {
+    viewAllData(orderPokeZa);
+} else if (resultado === "All") {
+    //       viewAllPokemon(allPokeData);
+}
+//    return menuPokedex;
+//});
